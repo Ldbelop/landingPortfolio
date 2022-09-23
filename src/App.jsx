6b2b8ba4b16c6fd5d04a81b405467668db9a1106
LeonarDev (David Barón) <baronavilandavid@gmail.com>
2022-09-22 23:40:38 -0500

    Primer commit, estructura básica, themes configurados

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { AppTheme } from './themes/AppTheme'
+
+function App() {
+
+  return (
+    <AppTheme>
+      <Button variant="contained" color="primary">
+        Hola mundo MUI
+      </Button>
+    </AppTheme>
+  )
+}
+
+export default App
---
+++ src/themes/MyTheme.jsx
@@ -0,0 +1,16 @@
+export const myTheme = createTheme({
+    palette: {
+        primary: {
+
+        },
+        second: {
+
+        },
+        terciary: {
+
+        },
+        error: {
+            
+        }
+    }
+})
