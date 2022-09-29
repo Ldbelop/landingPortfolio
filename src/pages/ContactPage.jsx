0fb1bb2ea1fd0009552ccfa6fe0bbfce38c1c964
LeonarDev (David Barón) <baronavilandavid@gmail.com>
2022-09-29 15:06:59 -0500

    probando bugfix

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,14 +4,29 @@ export default function ContactPage(){
     const [value, setValue] = useState("Escribe tu mensaje")
 
     function handleChange(event){
-        setValue(" ")
         setValue(event.target.value)
     }
+
+    fetch("https://formsubmit.co/ajax/your@email.com", {
+    method: "POST",
+    headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        name: "FormSubmit",
+        message: "I'm from Devro LABS"
+    })
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
     return <section className="px-[353px] py-[60px] h-auto">
     <div className=" overflow-hidden w-[1203px] h-[833px] rounded-[50px] flex flex-col justify-center items-center">
         <h1 className="bg-contactTitlebg w-full h-[13%] font-semibold text-[48px] text-whiteTxt flex items-center pl-[46px]">Contáctame</h1>
         <div className="bg-contactbg w-full h-[87%] py-[25px] px-[40px]">
-            <form action="https://formsubmit.co/320dc02e6126ba94c7a0ecaadc6b45b0" method="POST" >
+            <form action="https://formsubmit.co/baronavilandavid@gmail.com" method="POST" >
                 <div className="flex h-auto">
                     <label className="flex flex-col">
                         <p className="font-semibold text-[24px] text-whiteTxt pl-[33px] w-[100%]">Nombres</p>
@@ -30,6 +45,7 @@ export default function ContactPage(){
                     <p className="font-semibold text-[24px] text-whiteTxt pl-[33px] w-[100%]">Mensaje</p>
                     <textarea name="mensaje" className="mt-[23px] h-[213px] rounded-[20px] text-[#6A7384] pl-[33px]" value={value} onChange={handleChange} required/>
                 </label>
+
                 <button type="submit" className="w-[181px] h-[61px] rounded-[20px] mt-[45px] bg-blackBtn font-semibold text-whiteTxt text-[24px] flex justify-center items-center">Envíar <ion-icon name="send"></ion-icon></button>
             </form>
         </div>
